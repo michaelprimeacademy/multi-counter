f8ae54cf19daca6384706919a11db37e4d9cc22b
Michael Karas <michael_primeacademy@karas1.com>
2023-05-30 21:46:13 -0400

    added +5 and -5 buttons

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -16,10 +16,23 @@ export function Counter() {
         console.log('increment')
         setCount(count +1);
     }
+
+    const decreaseFive = () => {
+            console.log('decrement 5')
+            setCount(count -5);
+    }
+    const incrementFive = () => {
+                console.log('increment 5')
+                setCount(count +5);
+    }
+
+  
     return (
         <div>
             <Label count={count}/>
             <button onClick={increment}>Increase by 1</button>
+            <button onClick={incrementFive}>Increase by 5</button>
+            <button onClick={decreaseFive}>Decrease by 5</button>
         </div>
     )
 }
